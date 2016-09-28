d49e099f444d966003dc59146e91cc91b7c292e5
Ankush Goyal <goyalankush007@gmail.com>
2016-09-28 15:47:16 +0530

    Updated Code

--- utils/UrlShortern.js
+++ utils/UrlShortern.js
@@ -2,16 +2,16 @@ class UrlShortern {
 	
 	constructor() {
 		this.alphabet = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
-		this.base = alphabet.length; // base is the length of the alphabet (58 in this case)
+		this.base = this.alphabet.length; // base is the length of the alphabet (58 in this case)
 	}
 
 	// utility function to convert base 10 integer to base 58 string
 	encode(num){
   		var encoded = '';
   		while (num){
-    		var remainder = num % base;
-    		num = Math.floor(num / base);
-    		encoded = alphabet[remainder].toString() + encoded;
+    		var remainder = num % this.base;
+    		num = Math.floor(num / this.base);
+    		encoded = this.alphabet[remainder].toString() + encoded;
   		}
   		return encoded;
 	}
@@ -20,9 +20,9 @@ class UrlShortern {
 	decode(str){
   		var decoded = 0;
   		while (str){
-	    	var index = alphabet.indexOf(str[0]);
+	    	var index = this.alphabet.indexOf(str[0]);
 	    	var power = str.length - 1;
-	    	decoded += index * (Math.pow(base, power));
+	    	decoded += index * (Math.pow(this.base, power));
 	    	str = str.substring(1);
 	  	}
   		return decoded;
