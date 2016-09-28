3d69e3ca53d018761b1e46c53825bd12411d699b
Ankush Goyal <goyalankush007@gmail.com>
2016-09-28 16:57:30 +0530

    Updated Code

--- public/javascripts/shorten.js
+++ public/javascripts/shorten.js
@@ -1,25 +1,41 @@
 // add an event listener to the shorten button for when the user clicks it
 $('.btn-shorten').on('click', function(){
   // AJAX call to /api/shorten with the URL that the user entered in the input box
-  $.ajax({
-    url: '/api/urlshorten',
-    type: 'POST',
+  // $.ajax({
+  //   url: '/api/urlshorten',
+  //   type: 'POST',
+  //   headers: {
+  //       'Content-Type':'application/json'
+  //   },
+  //   data: {
+  //     'url': $('#url-field').val()
+  //   },
+  //   success: function(data){
+  //       // display the shortened URL to the user that is returned by the server
+  //       var resultHTML = '<a class="result" href="' + data + '">'
+  //           + data + '</a>';
+  //       $('#link').html(resultHTML);
+  //       $('#link').hide().fadeIn('slow');
+  //   },
+  //   error: function (textStatus, errorThrown) {
+  //       console.log(textStatus);
+  //   }
+  // });
+
+  console.log($('#url-field').val());
+  var settings = {
+    url: "/api/urlshorten",
+    method: "POST",
     headers: {
-        'Content-Type':'application/json'
+      "Content-Type": "application/json"
     },
     data: {
-      'url': $('#url-field').val()
-    },
-    success: function(data){
-        // display the shortened URL to the user that is returned by the server
-        var resultHTML = '<a class="result" href="' + data + '">'
-            + data + '</a>';
-        $('#link').html(resultHTML);
-        $('#link').hide().fadeIn('slow');
-    },
-    error: function (textStatus, errorThrown) {
-        console.log(textStatus);
+      url: "http://goyalzz.blogspot.com"
     }
+  }
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
   });
 
 });
