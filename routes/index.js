bfa6219168b2203721f6cbe9722560de7aab5cee
Ankush Goyal <goyalankush007@gmail.com>
2016-09-29 12:39:23 +0530

    Updated Code

--- routes/index.js
+++ routes/index.js
@@ -30,6 +30,11 @@ router.get(['/tinyurl', '/tinyurl.html'], function(req, res, next) {
   res.sendFile(path.resolve('views') + '/tinyurl.html');
 });
 
+router.get(['/install', '/install.html'], function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.sendFile(path.resolve('views') + '/installad.html');
+});
+
 // ALWAYS AT LAST POSITION
 router.get('/:encoded_id', function(req, res, next) {
   // check if url already exists in database
